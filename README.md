f4041615cc2b10d7da10c8e74c958c8c4b5691fd
Alexander Shepelin <brightcor@gmail.com>
2017-09-10 23:07:51 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub User Explorer
+
+My coding assignment for GrandCapital.
+
+## Scripts
+
+`npm start` – run webpack-dev-server
+
+`npm run build` – make production build 
---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {Form, FormGroup, FormControl, Button, PageHeader} from 'react-bootstrap';
+import UserList from './UserList/UserList';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+            users: []
+        };
+
+        this.onQueryChange = this.onQueryChange.bind(this);
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    onQueryChange(e) {
+        this.setState({
+            query: e.target.value
+        });
+    }
+
+    onSearch(e) {
+        e.preventDefault();
+        const URL = `https://api.github.com/search/users?type=all&sort=updated&per_page=100&q=${this.state.query}&page=1`;
+        fetch(URL).then((response) => {
+            return response.json();
+        }).then((response) => {
+            this.setState({
+                users: response.items
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className='container-fluid'>
+                <PageHeader>
+                    <a href="/">GitHub User Explorer</a>
+                </PageHeader>
+
+                <Form inline
+                      onSubmit={this.onSearch}>
+                    <FormGroup>
+                        <FormControl type="text"
+                                     placeholder="Search"
+                                     value={this.state.query}
+                                     onChange={this.onQueryChange}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <Button type="submit">Submit</Button>
+                </Form>
+
+                <div style={{marginTop: '24px'}}>
+                    <UserList list={this.state.users}
+                              onSelect={()=>{}}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
