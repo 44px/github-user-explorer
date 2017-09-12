fb8b36b22332c1c2fd1c067a490e89c28f1fdfd3
Alexander Shepelin <brightcor@gmail.com>
2017-09-13 00:00:02 +0300

    simple user page

--- src/user/UserPage.js
+++ src/user/UserPage.js
@@ -1,12 +1,66 @@
 import React, {Component} from 'react';
+import {Media} from 'react-bootstrap';
 
 export default class UserPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoading: true,
+            user: {}
+        }
+    }
+
+    componentDidMount() {
+        const {username} = this.props.match.params;
+        fetch(`https://api.github.com/users/${username}`).then((response) => {
+            if (!response.ok) {
+                throw new Error('Not found');
+            }
+            return response.json();
+        }).then((user) => {
+            this.setState({
+                isLoading: false,
+                user
+            });
+        }).catch((e) => {
+            this.setState({
+                isLoading: false,
+                user: {}
+            });
+        });
     }
 
     render() {
-        const {username} = this.props.match.params;
-        return <div>UserPage: {username}</div>
+        if (this.state.isLoading) {
+            return (
+                <div>Loading...</div>
+            );
+        }
+
+        const user = this.state.user;
+        if (!user.id) {
+            return (
+                <div>Not found</div>
+            );
+        }
+
+        return (
+            <Media>
+                <Media.Left align='top'>
+                    <img src={user.avatar_url}
+                         alt={user.login}
+                         width={128}
+                         height={128}
+                    />
+                </Media.Left>
+                <Media.Body>
+                    <Media.Heading>
+                        {`${user.login} ${user.name || ''}`}
+                    </Media.Heading>
+                    <div>Bio: {user.bio || '–'}</div>
+                    <div>Location: {user.location || '–'}</div>
+                </Media.Body>
+            </Media>
+        );
     }
 }
